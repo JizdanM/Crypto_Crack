9358f778771d20268fb2e08535ece3326c87fd96
mWarned <mihai.jizdan1@gmail.com>
2024-01-08 16:24:09 +0200

    Added a simple menu and file reading/writing

--- Python_Code/main.py
+++ Python_Code/main.py
@@ -1,12 +1,50 @@
+# This project was created with the help of the yt channel - https://www.youtube.com/@LearningPythonProgramming
+# As well as the book of the author (Al Sweigart) - https://nostarch.com/crackingcodes/
+
 import pyperclip
 import sys
 import vigenere
 import detectEnglish
 import vigenereDictionaryHacker  # Dictionary brute-force
+import vars
+import re
 
 
 def main():
-    translateVigenere('Llkj ml s wgtvxl qgjwtyi', 'SECRET', 'decrypt')
+    key = input('Key word please - ')  # Keyword input
+
+    # Silet Mode
+    if input('Do you want to execute the app in silent mode? (Y/N): ').upper().startswith('Y'):
+        vars.SILENT_MODE = True
+    else:
+        vars.SILENT_MODE = False
+
+    # Message reading and formating
+    inputFile = open('input.txt')
+    message = inputFile.read()
+    inputFile.close()
+
+    message = messageFormat(message)
+
+    menu = input('Choose the action:\n Encryption - E\n Decryption - D\n Hacking - H\n\n > ')
+
+    # Basic menu
+    if menu.upper().startswith('E'):
+        encryptMessage(message, key)
+    elif menu.upper().startswith('D'):
+        decryptMessage(message, key)
+    else:
+        crackVigenere(message)
+
+
+# Nested method for encryption
+def encryptMessage(message, key):
+    translateVigenere(message, key, 'encrypt')
+
+
+# Nested method for decription
+def decryptMessage(message, key):
+    translateVigenere(message, key, 'decrypt')
 
 
 # Function that encrypts and decrypts a message using the Vigenere cipher
@@ -18,13 +56,24 @@ def translateVigenere(message, key, mode):
         translated = vigenere.translateMessage(message, key, mode)
         print('The %sed message: ' % mode)
         print(translated)
-        pyperclip.copy(translated)
-        print('\n***\nMessage copied to the clipboard.\n***')
+        with open('output.txt', 'w') as file:
+            file.write(translated)
+        print('\n***\nMessage copied to output file.\n***')
+
+        if input('\nDo you wish to copy the message to the clipboard? (Y/N)').upper().startswith('Y'):
+            pyperclip.copy(translated)
+            print('\n***\nMessage copied to your clipboard.\n***')
 
 
-# Function that decrypts messages encrypted using the Vigenere function, without the need of a key
+# Decrypts messages encrypted using the Vigenere function, without the need of a key
 def crackVigenere(message):
-    return 0
+    return message
+
+
+# Formats the input message to get rid of any non-letter cahracters
+def messageFormat(message):
+    formated = re.sub(f'[^{re.escape(vars.FULL_LETTER_PATTERN)}\n]', '', message)
+    return formated
 
 
 def validMode(mode):
