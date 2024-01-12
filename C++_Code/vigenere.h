9ec9f125553e0760bca19d405783495654486bec
mWarned <mihai.jizdan1@gmail.com>
2024-01-12 15:43:38 +0200

    Allocated memory for variables in vigenere.h
    Variables:
    keyIndex and uppercaseKey

--- C++_Code/vigenere.h
+++ C++_Code/vigenere.h
@@ -8,16 +8,16 @@ const int MAX_LENGTH = 1000;
 string translateMessage(const string& message, const string& key, const string& mode) {
     string translated;
 
-    size_t keyIndex = 0;
-    string uppercaseKey(key);
-    transform(uppercaseKey.begin(), uppercaseKey.end(), uppercaseKey.begin(), ::toupper);
+    size_t* keyIndex = new size_t(0);
+    string* uppercaseKey = new string(key);
+    transform(uppercaseKey->begin(), uppercaseKey->end(), uppercaseKey->begin(), ::toupper);
 
     size_t i = 0;
     while (i < message.length()) {
         char symbol = message[i];
         size_t num = LETTERS.find(toupper(symbol));
         if (num != string::npos) {
-            int keyNum = LETTERS.find(uppercaseKey[keyIndex]);
+            int keyNum = LETTERS.find((*uppercaseKey)[*keyIndex]);
 
             if (mode == "encrypt") {
                 num += keyNum;
@@ -40,9 +40,9 @@ string translateMessage(const string& message, const string& key, const string& 
                 translated += symbol;
             }
 
-            keyIndex++;
-            if (keyIndex == uppercaseKey.length()) {
-                keyIndex = 0;
+            (*keyIndex)++;
+            if (*keyIndex == uppercaseKey->length()) {
+                *keyIndex = 0;
             }
         } else {
             translated += symbol;
@@ -50,6 +50,9 @@ string translateMessage(const string& message, const string& key, const string& 
 
         i++;
     }
+
+    delete keyIndex;
+    delete uppercaseKey;
 
     return translated;
 }
