8ea89b90b5003e1751c93482a73ffb17b5a405b4
mWarned <mihai.jizdan1@gmail.com>
2024-01-15 09:47:35 +0200

    Dynamically allocate low usetime variables in vigenere.h

--- C++_Code/vigenere.h
+++ C++_Code/vigenere.h
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-const int MAX_LENGTH = 1000;
-
 string translateMessage(const string& message, const string& key, const string& mode) {
     string translated;
 
@@ -14,30 +12,30 @@ string translateMessage(const string& message, const string& key, const string& 
 
     size_t i = 0;
     while (i < message.length()) {
-        char symbol = message[i];
-        size_t num = LETTERS.find(toupper(symbol));
-        if (num != string::npos) {
+        char* symbol = new char(message[i]);
+        size_t* num = new size_t(LETTERS.find(toupper(*symbol)));
+        if (*num != string::npos) {
             int keyNum = LETTERS.find((*uppercaseKey)[*keyIndex]);
 
             if (mode == "encrypt") {
-                num += keyNum;
+                *num += keyNum;
             } else if (mode == "decrypt") {
-                num -= keyNum;
-                num = (num + LETTERS.length()) % LETTERS.length();
+                *num -= keyNum;
+                *num = (*num + LETTERS.length()) % LETTERS.length();
             }
 
-            if (num < 0) {
-                num = static_cast<int>((num + LETTERS.length()) % LETTERS.length());
+            if (*num < 0) {
+                *num = static_cast<int>((*num + LETTERS.length()) % LETTERS.length());
             } else {
-                num %= LETTERS.length();
+                *num %= LETTERS.length();
             }
 
-            if (isupper(symbol)) {
-                translated += LETTERS[num];
-            } else if (islower(symbol)) {
-                translated += tolower(LETTERS[num]);
+            if (isupper(*symbol)) {
+                translated += LETTERS[*num];
+            } else if (islower(*symbol)) {
+                translated += tolower(LETTERS[*num]);
             } else {
-                translated += symbol;
+                translated += *symbol;
             }
 
             (*keyIndex)++;
@@ -45,9 +43,11 @@ string translateMessage(const string& message, const string& key, const string& 
                 *keyIndex = 0;
             }
         } else {
-            translated += symbol;
+            translated += *symbol;
         }
 
+		delete symbol;
+		delete num;
         i++;
     }
 
